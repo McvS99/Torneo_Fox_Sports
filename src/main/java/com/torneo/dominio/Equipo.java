d31cb2295dbcaf853c7b2b182b5f48316ca689d9
Marlon Castrillon <marlonv203@gmail.com>
2019-03-28 21:42:03 -0500

    Creaciòn clase Equipo

--- src/main/java/com/torneo/dominio/Equipo.java
+++ src/main/java/com/torneo/dominio/Equipo.java
@@ -1,5 +1,86 @@
 package com.torneo.dominio;
 
+import java.util.ArrayList;
+
 public class Equipo {
 
+	private String nombre;
+	private byte puntosGanados = 0;
+	private ArrayList<Jugador> jugadores;
+
+	public Equipo(String nombre, ArrayList<Jugador> jugadores) {
+		this.nombre = nombre;
+		if (jugadores.size() > 16) {
+
+		}
+		if (jugadores.size() < 7) {
+			System.out.println("Se necesitan minimo 7 jugadores ");
+		} else {
+			this.jugadores = jugadores;
+		}
+	}
+
+	public ArrayList<String> verJugadores() {
+		ArrayList<String> listaJugadores = new ArrayList<String>();
+		for (int i = 0; i < this.jugadores.size(); i++) {
+			listaJugadores.add(this.jugadores.get(i).getNombre());
+			listaJugadores.add(this.jugadores.get(i).getPosicion());
+		}
+		return listaJugadores;
+
+	}
+
+	public ArrayList<String> verJugadoresPorPosicion(String posicion) {
+		ArrayList<String> listaJugadores = new ArrayList<String>();
+		for (int i = 0; i < this.jugadores.size(); i++) {
+			if (this.jugadores.get(i).getPosicion().contentEquals(posicion)) {
+				listaJugadores.add(this.jugadores.get(i).getNombre());
+			}
+		}
+		return listaJugadores;
+	}
+
+	public boolean agregarJugador(String nombre, String posicion) {
+		boolean seAgregoJugador = true;
+		if (this.jugadores.size() <= 16) {
+			this.jugadores.get(this.jugadores.size()).setNombre(nombre);
+			this.jugadores.get(this.jugadores.size()).setPosicion(posicion);
+		} else {
+			seAgregoJugador = false;
+		}
+		return seAgregoJugador;
+	}
+
+	public boolean retirarJugador(String nombre, String posicion) {
+		boolean seRetiroJugador = false;
+		for (int i = 0; i < this.jugadores.size(); i++) {
+			if ((this.jugadores.get(i).getNombre().contentEquals(nombre))
+					&& (this.jugadores.get(i).getPosicion().contentEquals(posicion))) {
+				this.jugadores.remove(i);
+				seRetiroJugador = true;
+			}
+		}
+		return seRetiroJugador;
+
+	}
+
+	public void agregarPuntos(byte puntos) {
+		this.puntosGanados += puntos;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public byte getPuntosGanados() {
+		return puntosGanados;
+	}
+
+	public void setJugadores(ArrayList<Jugador> jugadores) {
+		this.jugadores = jugadores;
+	}
 }
